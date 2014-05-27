9bbb4077516b362c964f5ce82256c8de0c594ba1
Edmund Wilson -uni <bowenjuice@gmail.com>
2014-05-27 17:02:45 +1200

    Taz Dingo

--- Piece.java
+++ Piece.java
@@ -1,7 +1,28 @@
 public class Piece {
 	int id;
 	Side[] side;
-	int nSides;
+	private int nSides;
+	private int noOfPlaces;
+	private int timesRotated
+	
+
+	public static int getNSides(){
+		return nSides;
+	}
+
+	public static int getNoOfPlaces(){
+		return noOfPlaces;
+	}
+	public static void setNoOfPlaces(int noOfPlaces){
+		noOfPlaces = this.noOfPlaces;
+	}
+
+	public static void setTimesRotated(int times){//Set times rotated clockwise
+		timesRotated = times;
+	}
+	public static int getTimesRotated(){
+		return timesRotated;
+	}
 
 	   static int piece_count = 0;
 	   static final Side[] prototype = new Side[0];
--- animalPuzzle.java
+++ animalPuzzle.java
@@ -1,35 +1,68 @@
+import java.util.Comparator;
 
 public class animalPuzzle{
-	Arraylist<Piece> puzzlepieces = new ArrayList<Piece>();
+	static private Comparator<Piece> ascPlacements;
+	static Arraylist<Piece> puzzlepieces = new ArrayList<Piece>();
+	static Boolean success = false;	
+	static {
+		asPlacements = new Comparator<Piece>(){
+			@Override
+			public int compare(Piece p1, Piece p2){
+				return p1.getNoOfPlaces().compareTo(p2.getNoOfPlaces());
+			}
+		};
+	}
 
-	private static int numberOfPlacements(Piece piece){
-		int count = 0;
+	
+	private static int numberOfPlacements(Piece piece, Piece[][] puzzle){
+		piece.setTimesRotated(0);
+		for(int i = 0; i < puzzle.length; i++){
+			for (int j = 0; j < puzzle[0].length; j++){
+				if(puzzle[i][j]==Null){ // every empty piece
+					
+					if(!puzzle[i+1][j]==Null || !puzzle[i-1][j]==Null //If atleast one neighbour
+					|| !puzzle[i][j-1]==Null || !puzzle[i][j+1]==Null){ // Is full
+						for(int side = 0; side < piece.getNSides(); side++){
+						
+							if(piece[side].equals
 
 
 	}
 
 
-	private static void place(){
-		int totalplacements;
+	private static Piece[][] place (Piece piece, Piece[][] puzzle){
+		
+	}
 
+
+	private static Boolean solve(Piece[][] puzzle){
+		int totalplacements;
+		Piece tempPiece;
 		if (puzzlepieces.isEmpty()){
-			return; // Success
+			return true; // Success
 		}
 
 		totalplacements = 0;
 		for (piece : puzzlepieces){
-			piece.setNumberOfPlacements(numberOfPlacements(piece));
-			totalplacements += numberOfPlacements(piece);//
+			piece.setNoOfPlaces(numberOfPlacements(piece,puzzle));
+			totalplacements += numberOfPlacements(piece,puzzle);//
 		}			
 		
 		if (totalplacements == 0){
-			return; // failure
+			return false; // failure
 		}
 		
-		//Sort By NumberOfPlacements
+		Collections.sort(puzzlepieces, ascPlacements);
+
+		tempPiece = puzzlepieces.remove(0);
 		
-
-
+		
+		for (int i = 0; i < tempPiece.getNoOfPlaces(); i++){ //needs to solve for certain rotations too
+		if(solve(place(tempPiece,puzzle,i))){
+			return true; 
+		}
+		puzzlepieces.add(tempPiece);
+		return false;
 	}
 
 
