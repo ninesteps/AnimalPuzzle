92abf24b8243666e13e6c71da4c2df69a4013553
Edmund Wilson -uni <bowenjuice@gmail.com>
2014-05-27 14:38:21 +1200

    Multilayer Perceptron

--- Piece.java
+++ Piece.java
@@ -2,4 +2,12 @@ public class Piece {
 	int id;
 	Side[] side;
 	int nSides;
+
+	   static int piece_count = 0;
+	   static final Side[] prototype = new Side[0];
+	   public Piece(ArrayList<Side> some_sides) {
+		   id = ++piece_count;
+		   side = some_sides.toArray(prototype);
+		   nSides = side.length;
+	   }
 }
